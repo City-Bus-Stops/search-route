04fbd8b8a1d792f7d5d08c2590410874afa95c96
KrivichaninDenis <krivichaninds@gmail.com>
2017-03-26 00:11:49 +0300

    Update FoundedRouteInfo

--- src/components/FoundedRouteInfo/FoundedRouteInfo.jsx
+++ src/components/FoundedRouteInfo/FoundedRouteInfo.jsx
@@ -9,13 +9,14 @@ const FoundedRouteInfo = () => (
     closeOnEscape={false}
     closeOnRootNodeClick={false}
   >
+    <i className="ui blue left corner label" />
     <Modal.Header as="h3" className="ui">
       <div className="ui grid">
         <div className="two column row">
           <div className="column">
-            <h3 className="ui header blue">
+            <h2 className="ui header green">
               Фолюш
-            </h3>
+            </h2>
           </div>
           <div className="right aligned column">
             <div
@@ -34,24 +35,55 @@ const FoundedRouteInfo = () => (
     </Modal.Header>
     <Modal.Content>
       <div className="ui grid">
-        <div className="one column row">
-          <div className="twelve wide column">
-            <h3 className="ui header blue">
-              From:
-            </h3>
-            Улица О.Соломовой 135
+        <div className="ui large steps fluid">
+          <div className="step">
+            <i className="user icon" />
+            <div className="content">
+              <div className="title">From</div>
+              <div className="description">Улица О.Соломовой 135</div>
+            </div>
+          </div>
+          <div className="step">
+            <i className="flag checkered icon" />
+            <div className="content">
+              <div className="title">To</div>
+              <div className="description">Проспект Янки Купалы 26а</div>
+            </div>
           </div>
         </div>
         <div className="one column row">
-          <div className="twelve wide column">
-            <h3 className="ui header blue">
-              To:
-            </h3>
-            Проспект Янки Купалы 26а
+          <div className="sixteen wide column">
+            <div className="ui basic segment">
+              <div className="ui big relaxed divided list">
+                <div className="item">
+                  <div className="content">
+                    <div className="header">Улица Поповича</div>
+                    2 min
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="content">
+                    <div className="header">Улица Декабристов</div>
+                    4 min
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="content">
+                    <div className="header">Улица Горновых</div>
+                      7 min
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </Modal.Content>
+    <div className="actions">
+      <h3 className="ui header blue">
+        Bus stops count: 7
+      </h3>
+    </div>
   </Modal>
 );
 
