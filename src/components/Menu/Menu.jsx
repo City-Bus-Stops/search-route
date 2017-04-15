955059af38eb64ba354361af047bfffdc0b7576d
KrivichaninDenis <krivichaninds@gmail.com>
2017-04-15 16:04:10 +0300

    Add isAuth property to Menu component. Add prop-types package

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,13 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import Collapse from 'react-collapse';
 
-const Menu = ({ pathname }) => (
+const Menu = ({ pathname, isUserAuth, userCredentials }) => (
   <Collapse isOpened={!pathname.includes('map')}>
     <div className="ui vertical center aligned small padded segment">
       <div className="ui container">
-        <div className="ui large secondary menu">
+        <div className="ui huge secondary menu">
           <Link to="/dashboard" className="ui item" activeClassName="ui active item">
             <i className="home icon" />
             Home
@@ -19,16 +20,27 @@ const Menu = ({ pathname }) => (
             <i className="world icon" />
             Map
           </Link>
-          <div className="right menu">
-            <Link to="/login" className="ui item" activeClassName="ui active item">
-              <i className="sign in icon red" />
-              Log In
-            </Link>
-            <Link to="/signup" className="ui item" activeClassName="ui active item">
-              <i className="user outline icon" />
-              Sign Up
-            </Link>
-          </div>
+            {
+              isUserAuth ?
+                <div className="right menu">
+                  <div className="ui item">
+                    {`${userCredentials.firstName} ${userCredentials.lastName}`}
+                    <Link to="/logout">
+                      <i className="sign out icon link red" />
+                    </Link>
+                  </div>
+                </div> :
+                <div className="right menu">
+                  <Link to="/login" className="ui item" activeClassName="ui active item">
+                    <i className="sign in icon red" />
+                    Log In
+                  </Link>
+                  <Link to="/signup" className="ui item" activeClassName="ui active item">
+                    <i className="user outline icon" />
+                    Sign Up
+                  </Link>
+                </div>
+            }
         </div>
       </div>
     </div>
@@ -37,6 +49,16 @@ const Menu = ({ pathname }) => (
 
 Menu.propTypes = {
   pathname: PropTypes.string.isRequired,
+  isUserAuth: PropTypes.bool,
+  userCredentials: PropTypes.shape(),
 };
+
+Menu.defaultProps = {
+  isUserAuth: false,
+  userCredentials: {
+    firstName: '',
+    lastName: '',
+  },
+}
 
 export default Menu;
