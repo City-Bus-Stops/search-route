2414a430fb97c05b53c44bb5aa5b57b91e71dd57
KrivichaninDenis <krivichaninds@gmail.com>
2017-04-16 20:03:39 +0300

    Add PointInfo component

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,18 @@
 import L from 'leaflet';
 
 export const markerTypes = {
-  start_point: { icon: 'avatar', color: 'white', background: '#388E3C', size: 35 },
+  start: { icon: 'avatar', color: 'white', background: '#388E3C', size: 35 },
   bus_stop: { icon: 'bus', color: 'white', background: '#3949AB', size: 35 },
-  end_point: { icon: 'golf', color: '#C62828', background: 'white', size: 35 },
+  end: { icon: 'golf', color: '#C62828', background: 'white', size: 35 },
 };
 
-export const generateMarker = markerType => L.icon.mapkey(markerTypes[markerType]);
+export const generateIcon = markerType => L.icon.mapkey(markerTypes[markerType]);
 
-export const pointToLayer = (feature, latlng) =>
-  L.marker(latlng, { icon: generateMarker(feature.properties.type) });
+export const generateMarker = (feature, latlng, onClick) => {
+  const { properties } = feature;
+  const { id, name } = properties;
+  const marker = L.marker(latlng, { icon: generateIcon(feature.properties.type) });
+  return properties.type === 'start' || properties.type === 'end' ?
+    marker.bindPopup(properties.address) :
+    marker.on('click', () => onClick(id, name));
+};
