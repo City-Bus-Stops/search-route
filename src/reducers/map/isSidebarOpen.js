630df3bf8995614d7999df01ee93507bca8bc203
KrivichaninDenis <krivichaninds@gmail.com>
2017-05-13 22:11:34 +0300

    Close sidebar when nearest bus stops were loaded

--- src/reducers/map/isSidebarOpen.js
+++ src/reducers/map/isSidebarOpen.js
@@ -1,11 +1,15 @@
 import {
   TOGGLE_SIDEBAR,
+  FIND_NEAREST_BUS_STOPS_SUCCESS,
 } from '../../actions/actions';
 
 const isSidebarOpen = (state = false, action) => {
   switch (action.type) {
     case TOGGLE_SIDEBAR:
       return !state;
+
+    case FIND_NEAREST_BUS_STOPS_SUCCESS:
+      return false;
 
     default:
       return state;
